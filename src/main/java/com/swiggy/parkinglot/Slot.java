7652391fd8ae5749dbfaaf5b74e3c892c9698761
dhruvmehta-swiggy <dhruv.mehta_ftc@external.swiggy.in>
2025-02-04 11:17:52 +0530

    Refactor Slot for better encapsulation

--- src/main/java/com/swiggy/parkinglot/Slot.java
+++ src/main/java/com/swiggy/parkinglot/Slot.java
@@ -1,21 +1,12 @@
 package com.swiggy.parkinglot;
 
 import com.swiggy.parkinglot.vehicle.Vehicle;
-import com.swiggy.parkinglot.vehicle.VehicleColor;
-
-import java.util.List;
 import java.util.Objects;
 
 public class Slot {
-    private final int distanceFromEntrance;
     private Vehicle parkedVehicle;
 
-    public Slot(int distanceFromEntrance) {
-        if (distanceFromEntrance < 0) {
-            throw new IllegalArgumentException("Distance cannot be negative");
-        }
-
-        this.distanceFromEntrance = distanceFromEntrance;
+    public Slot() {
         this.parkedVehicle = null;
     }
 
@@ -37,53 +28,8 @@ public class Slot {
         parkedVehicle = null;
     }
 
-    // Method to find the nearest slot from the entrance
-    public static Slot findNearestSlot(List<Slot> slots) {
-        if (slots == null || slots.isEmpty()) {
-            throw new IllegalArgumentException("Slots cannot be null or empty");
-        }
-
-        Slot nearestSlot = null;
-        for (Slot slot : slots) {
-            if (slot.parkedVehicle == null) {
-                if (nearestSlot == null || slot.distanceFromEntrance < nearestSlot.distanceFromEntrance) {
-                    nearestSlot = slot;
-                }
-            }
-        }
-
-        return nearestSlot;
-    }
-
-    // Method to get the slot where the vehicle is parked by registration number
-    public static Slot fetchSlotByRegistrationNumber(List<Slot> slots, String registrationNumber) {
-        if (registrationNumber == null || registrationNumber.isEmpty()) {
-            throw new IllegalArgumentException("Registration Number cannot be null or empty");
-        }
-
-        for (Slot slot : slots) {
-            if (slot.parkedVehicle != null && slot.parkedVehicle.hasSameRegistrationNumber(registrationNumber)) {
-                return slot;
-            }
-        }
-
-        return null;
-    }
-
-    // Method to get number of cars with given color
-    public static int fetchNumberOfCarsWithColor(List<Slot> slots, VehicleColor color) {
-        if (slots == null || slots.isEmpty()) {
-            throw new IllegalArgumentException("Slots cannot be null or empty");
-        }
-
-        int count = 0;
-        for (Slot slot : slots) {
-            if (slot.parkedVehicle != null && slot.parkedVehicle.hasSameColor(color)) {
-                count++;
-            }
-        }
-
-        return count;
+    public boolean isOccupied() {
+        return parkedVehicle != null;
     }
 
     @Override
@@ -95,6 +41,6 @@ public class Slot {
             return false;
         }
         Slot slot = (Slot) obj;
-        return distanceFromEntrance == slot.distanceFromEntrance && Objects.equals(parkedVehicle, slot.parkedVehicle);
+        return Objects.equals(parkedVehicle, slot.parkedVehicle);
     }
 }
--- src/test/java/com/swiggy/parkinglot/SlotTest.java
+++ src/test/java/com/swiggy/parkinglot/SlotTest.java
@@ -4,47 +4,15 @@ import com.swiggy.parkinglot.vehicle.Vehicle;
 import com.swiggy.parkinglot.vehicle.VehicleColor;
 import com.swiggy.parkinglot.vehicle.VehicleType;
 import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SlotTest {
 
-    // Test to check equals method when different objects with same values are compared
-    @Test
-    public void testEquals_WhenSameValues_ThenTrue() {
-        Slot slot1 = new Slot(1);
-        Slot slot2 = new Slot(1);
-        assertEquals(slot1, slot2);
-    }
-
-    // Test to check equals method when different objects are compared
-    @Test
-    public void testEquals_WhenDifferentObjects_ThenFalse() {
-        Slot slot1 = new Slot(1);
-        Slot slot2 = new Slot(2);
-        assertNotEquals(slot1, slot2);
-    }
-
-    // Test to check constructor when distance is -1
-    @Test
-    public void testConstructor_WhenDistanceMinus1_ThrowsIllegalArgumentException() {
-        assertThrows(IllegalArgumentException.class, () -> new Slot(-1));
-    }
-
-    // Test to check constructor when distance is -3
-    @Test
-    public void testConstructor_WhenDistanceIsNegative_ThrowsIllegalArgumentException() {
-        assertThrows(IllegalArgumentException.class, () -> new Slot(-3));
-    }
-
     // Test to check park method when slot is not vacant
     @Test
-    public void testPark_WhenSlotIsNotVacant_ThrowsIllegalStateException() {
+    public void testPark_WhenSlotIsOccupied_ThrowsIllegalStateException() {
         Vehicle car = new Vehicle("KA-01-HH-1234", VehicleColor.WHITE, VehicleType.CAR);
-        Slot slot = new Slot(1);
+        Slot slot = new Slot();
 
         assertThrows(IllegalStateException.class, () -> {
             slot.park(car);
@@ -55,148 +23,34 @@ public class SlotTest {
     // Test to check un-park method when slot is already vacant
     @Test
     public void testUnpark_WhenSlotIsAlreadyVacant_ThrowsIllegalStateException() {
-        Slot slot = new Slot(1);
+        Slot slot = new Slot();
 
         assertThrows(IllegalStateException.class, slot::unpark);
     }
 
-    // Test to check findNearestSlot method when slots is null
+    // Test to check equals method when different objects with same values are compared
     @Test
-    public void testFindNearestSlot_WhenAllSlotsNull_ThrowsIllegalArgumentException() {
-        assertThrows(IllegalArgumentException.class, () -> Slot.findNearestSlot(null));
+    public void testEquals_WhenSameValues_ThenTrue() {
+        Vehicle car = new Vehicle("KA-01-HH-1234", VehicleColor.WHITE, VehicleType.CAR);
+        Slot slot1 = new Slot();
+        slot1.park(car);
+        Slot slot2 = new Slot();
+        slot2.park(car);
+
+        assertEquals(slot1, slot2);
     }
 
-    // Test to check findNearestSlot method when all slots are vacant
+    // Test to check equals method when different objects are compared
     @Test
-    public void testFindNearestSlot_WhenAllSlotsVacant_ThenFirstSlot() {
-        List<Slot> slots = new ArrayList<>();
-        Slot slot1 = new Slot(1);
-        Slot slot2 = new Slot(2);
-
-        slots.add(slot1);
-        slots.add(slot2);
-
-        Slot expected = Slot.findNearestSlot(slots);
-        assertEquals(slot1, expected);
-    }
-
-    // Test to check findNearestSlot method when all slots are full
-    @Test
-    public void testFindNearestSlot_WhenAllSlotsFull_ThenNull() {
-        List<Slot> slots = new ArrayList<>();
-        Slot slot1 = new Slot(1);
-        Slot slot2 = new Slot(2);
+    public void testEquals_WhenDifferentObjects_ThenFalse() {
         Vehicle car1 = new Vehicle("KA-01-HH-1234", VehicleColor.WHITE, VehicleType.CAR);
-        Vehicle car2 = new Vehicle("KA-01-HH-1235", VehicleColor.WHITE, VehicleType.CAR);
-
+        Slot slot1 = new Slot();
         slot1.park(car1);
+        Vehicle car2 = new Vehicle("KA-01-HH-1235", VehicleColor.BLACK, VehicleType.CAR);
+        Slot slot2 = new Slot();
         slot2.park(car2);
-        slots.add(slot1);
-        slots.add(slot2);
 
-        Slot expected = Slot.findNearestSlot(slots);
-        assertNull(expected);
-    }
-
-    // Test to check fetchNumberOfCarsWithColor method when slots is null
-    @Test
-    public void testFetchNumberOfCarsWithColor_WhenAllSlotsNull_ThrowsIllegalArgumentException() {
-        assertThrows(IllegalArgumentException.class, () -> Slot.fetchNumberOfCarsWithColor(null, VehicleColor.WHITE));
-    }
-
-    // Test to check fetchNumberOfCarsWithColor method when no black car is present
-    @Test
-    public void testFetchNumberOfCarsWithColor_WhenAllSlotsVacant_ThenZero() {
-        List<Slot> slots = new ArrayList<>();
-        Slot slot1 = new Slot(1);
-        Slot slot2 = new Slot(2);
-        Vehicle car1 = new Vehicle("KA-01-HH-1234", VehicleColor.WHITE, VehicleType.CAR);
-        Vehicle car2 = new Vehicle("KA-01-HH-1235", VehicleColor.BLUE, VehicleType.CAR);
-
-        slot1.park(car1);
-        slot2.park(car2);
-        slots.add(slot1);
-        slots.add(slot2);
-
-        int expected = Slot.fetchNumberOfCarsWithColor(slots, VehicleColor.BLACK);
-        assertEquals(0, expected);
-    }
-
-    // Test to check fetchNumberOfCarsWithColor method when two slots have white cars
-    @Test
-    public void testFetchNumberOfCarsWithColor_WhenTwoWhiteCars_ThenTwo() {
-        List<Slot> slots = new ArrayList<>();
-        Slot slot1 = new Slot(1);
-        Slot slot2 = new Slot(2);
-        Vehicle car1 = new Vehicle("KA-01-HH-1234", VehicleColor.WHITE, VehicleType.CAR);
-        Vehicle car2 = new Vehicle("KA-01-HH-1235", VehicleColor.WHITE, VehicleType.CAR);
-
-        slot1.park(car1);
-        slot2.park(car2);
-        slots.add(slot1);
-        slots.add(slot2);
-
-        int expected = Slot.fetchNumberOfCarsWithColor(slots, VehicleColor.WHITE);
-        assertEquals(2, expected);
-    }
-
-    // Test to check getSlotByRegistrationNumber method when registration number is null
-    @Test
-    public void testFetchSlotByRegistrationNumber_WhenRegistrationNumberIsNull_ThrowsIllegalArgumentException() {
-        // Arrange
-        List<Slot> slots = new ArrayList<>();
-        slots.add(new Slot(0));
-        slots.add(new Slot(1));
-
-        assertThrows(IllegalArgumentException.class, () -> Slot.fetchSlotByRegistrationNumber(slots, null));
-    }
-
-    // Test to check getSlotByRegistrationNumber method when registration number is empty
-    @Test
-    public void testFetchSlotByRegistrationNumber_WhenRegistrationNumberIsEmpty_ThrowsIllegalArgumentException() {
-        // Arrange
-        List<Slot> slots = new ArrayList<>();
-        slots.add(new Slot(0));
-        slots.add(new Slot(1));
-
-        assertThrows(IllegalArgumentException.class, () -> Slot.fetchSlotByRegistrationNumber(slots, ""));
-    }
-
-    // Test to check getSlotByRegistrationNumber method when registration number is not found
-    @Test
-    public void testFetchSlotByRegistrationNumber_WhenRegistrationNumberNotFound_ThenNull() {
-        // Arrange
-        List<Slot> slots = new ArrayList<>();
-        Slot slot1 = new Slot(0);
-        Slot slot2 = new Slot(1);
-        Vehicle car1 = new Vehicle("ABC123", VehicleColor.BROWN, VehicleType.CAR);
-        Vehicle car2 = new Vehicle("ABC000", VehicleColor.BLACK, VehicleType.CAR);
-        slot1.park(car1);
-        slots.add(slot1);
-        slot2.park(car2);
-        slots.add(slot2);
-
-        Slot expectedSlot = Slot.fetchSlotByRegistrationNumber(slots, "XYZ123");
-
-        assertNull(expectedSlot);
-    }
-
-    // Test to check getSlotByRegistrationNumber method when registration number is found
-    @Test
-    public void testFetchSlotByRegistrationNumber_WhenRegistrationNumberFound_ThenSlot() {
-        List<Slot> slots = new ArrayList<>();
-        Slot slot1 = new Slot(0);
-        Slot slot2 = new Slot(1);
-        Vehicle car1 = new Vehicle("ABC123", VehicleColor.BROWN, VehicleType.CAR);
-        Vehicle car2 = new Vehicle("ABC000", VehicleColor.BLACK, VehicleType.CAR);
-        slot1.park(car1);
-        slots.add(slot1);
-        slot2.park(car2);
-        slots.add(slot2);
-
-        Slot expectedSlot = Slot.fetchSlotByRegistrationNumber(slots, "ABC000");
-
-        assertEquals(expectedSlot, slot2);
+        assertNotEquals(slot1, slot2);
     }
 
 }
